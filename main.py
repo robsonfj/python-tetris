3e34aa7f17b244888be9c86875b7086adde805f7
Robson <Robson.ferreiraj@outlook.com>
2018-11-09 21:15:29 -0200

    jogo iniciando com teclas na tela e texto

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from cocos.director import director
 
 pyglet.resource.path = ['assets', 'assets/blocks'] # caminho para imagens e sprites
 pyglet.resource.reindex()
-
 def run(wind_width=800, wind_height=600):
     print("Start Game")
     director.init(width=wind_width, height=wind_height, caption="TETRIS", fullscreen=False)
--- main_game_scene.py
+++ main_game_scene.py
@@ -1,6 +1,7 @@
 from cocos.layer import Layer
 from cocos.scene import Scene
 from piece_layer import Piece
+from help_layer import Keys
 
 class Main_Game(Scene):
     def __init__(self, lines, columns):
@@ -11,10 +12,13 @@ class Main_Game(Scene):
 
         piece = Piece((100,300), "flat")
         self.add(piece)
-        
+
         piece1 = Piece((100,200), "flat")
         self.add(piece1)
 
         piece2 = Piece((100,100), "flat")
         self.add(piece2)
+
+        keys_help = Keys((100,100))
+        self.add(keys_help)
         
