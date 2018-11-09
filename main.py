d2a9c69aaf18169f111dfe5f83457fb841f485ee
robsonfj <Robson.ferreiraj@outlook.com>
2018-11-09 01:27:17 -0200

    melhorado caminho para imagens

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from menu import Tetris_Menu
+import pyglet
 from cocos.director import director   
 
-
+pyglet.resource.path = ['assets', 'assets/blocks'] # caminho para imagens e sprites
+pyglet.resource.reindex()
 def run(wind_width=800, wind_height=600):
     print("Start Game")
     director.init(width=wind_width, height=wind_height, caption="TETRIS", fullscreen=False)
