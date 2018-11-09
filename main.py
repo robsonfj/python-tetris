6dfcb53ff4fda53e9a96a8563a2c86f1a7a144e6
Robson <Robson.ferreiraj@outlook.com>
2018-11-09 21:18:37 -0200

    importando fonte para o jogo

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from cocos.director import director
 
 pyglet.resource.path = ['assets', 'assets/blocks'] # caminho para imagens e sprites
 pyglet.resource.reindex()
+pyglet.font.add_file('./assets/tetrominoes.ttf')
+
 def run(wind_width=800, wind_height=600):
     print("Start Game")
     director.init(width=wind_width, height=wind_height, caption="TETRIS", fullscreen=False)
