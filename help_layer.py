9eeb62b32c13a8d1e434903c2b815d4f5a9c2ac5
Luciano <Luciano.d98@sempreceub.com>
2018-11-09 22:23:57 -0200

    Novas teclas

--- help_layer.py
+++ help_layer.py
@@ -6,20 +6,18 @@ from cocos.text import Label
 class Keys(Layer):
     def __init__(self,position):
         Layer.__init__(self)
-        arrows = Sprite(image=pyglet.resource.image('keyboard-arrows.png') , position=position, scale=0.4)
-        arrows.position = position
-        enter = Sprite(image=pyglet.resource.image('enter-button.png') , position=position, scale=0.2)
-        enter.position = (position[0]+ arrows.width + 20, position[1] + arrows.height + 40)
-        label_enter = Label("Girar peca", position=(enter.position[0] + enter.width + 5, enter.position[1]), font_name="Tetrominoes")
-        label_arrow_down = Label("Acelerar peca para baixo", position=(arrows.position[0] , arrows.position[1]))
-        label_arrow_left = Label("Mover para esquerda", position=(arrows.position[0] , arrows.position[1]))
-        label_arrow_right = Label("Mover para direita", position=(arrows.position[0] , arrows.position[1]))
+        arrows = Sprite(image=pyglet.resource.image('all-keys.png') , position=position, scale=0.5)
+        arrows.position = (position[0]+ arrows.width + 500, position[1] + arrows.height + -60)
+        label_enter = Label("Girar peca", position=(arrows.position[0] + arrows.width + -220, arrows.position[1] + arrows.height + -7), font_name="Tetrominoes")
+        label_arrow_down = Label("Acelerar peca para baixo", position=(arrows.position[0] + arrows.width -270 , arrows.position[1] + arrows.height + -170), font_name="Tetrominoes")
+        label_arrow_left = Label("Mover para esquerda", position=(arrows.position[0] , arrows.position[1]), font_name="Tetrominoes")
+        label_arrow_right = Label("Mover para direita", position=(arrows.position[0] , arrows.position[1]), font_name="Tetrominoes")
         
         self.add(label_enter)
         self.add(label_arrow_down)
         self.add(label_arrow_left)
         self.add(label_arrow_right)
         self.add(arrows)
-        self.add(enter)
+
 
         
